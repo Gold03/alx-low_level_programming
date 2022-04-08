7cb60242cc49fb1333d8781e2bc310a292105aab
Tife_dev <julietphilip2000@gmail.com>
2022-04-08 06:12:39 -0700

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,19 +10,12 @@ int main(void)
 {
 	char c = 'a';
 
-	while (c <= 'z')
-
-	{
-		putchar(c);
-		c++;
-	}
-	c = 'A';
-
 	while (c <= 'Z')
 	{
 		putchar(c);
 		c++;
 	}
+
 	putchar('\n');
 	return (0);
 }
