923cb2a6941d0cc6f0c401a5d3b8b529df762c74
Tife_dev <julietphilip2000@gmail.com>
2022-05-05 04:19:18 -0700

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -5,7 +5,7 @@
  *  space in memory which contains a copy of the string
  *  passed.
  *  @str: pointer to string being duplicated.
- *   
+ *
  *   Return: NULL if str is NULL.
  *   pointer to duplicated string on success.
  *   NULL if memory was insufficient.
